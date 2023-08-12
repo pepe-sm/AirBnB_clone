5a1a2297a1517b211b560e72e7998055013cec61
EdwinKamau2 <edwinkamau2@gmail.com>
2023-08-12 09:20:50 -0700

    more classes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This is the amenity module
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ This is the Amenity class """
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This is the city module
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Is the City class """
+
+    state_id = ""
+    name = ""
